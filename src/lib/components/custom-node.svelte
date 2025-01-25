498774132a272522b85f4b826c04c1909ea43f99
Niklas Hansson <niklashansson97@gmail.com>
2025-01-25 12:53:35 +0100

    add svelteflow

---
+++ src/lib/components/custom-node.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+    import { Handle, Position, type NodeProps } from '@xyflow/svelte';
+   
+    let props: NodeProps = $props();
+
+
+                    
+
+  </script>
+   
+  <div data-selected={props.selected} class="p-12 bg-white border-black border rounded flex items-center justify-center data-[selected=true]:ring-2">
+    <Handle type="target" position={Position.Left} />
+    <div class="nodrag">Should not select</div>
+    <Handle type="source" position={Position.Right} />
+  </div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,65 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+    import { writable } from 'svelte/store';
+    import {
+      SvelteFlow,
+      Controls,
+      Background,
+      BackgroundVariant,
+      MiniMap
+    } from '@xyflow/svelte';
+   
+    // 👇 this is important! You need to import the styles for Svelte Flow to work
+    import '@xyflow/svelte/dist/style.css';
+  import CustomNode from '$lib/components/custom-node.svelte';
+   
+    // We are using writables for the nodes and edges to sync them easily. When a user drags a node for example, Svelte Flow updates its position.
+    const nodes = writable([
+      {
+        id: '1',
+        type: 'custom',
+        data: { label: 'Input Node' },
+        position: { x: 0, y: 0 }
+      },
+      {
+        id: '2',
+        type: 'custom',
+        data: { label: 'Node' },
+        position: { x: 0, y: 150 }
+      }
+    ]);
+   
+    // same for edges
+    const edges = writable([
+      {
+        id: '1-2',
+        type: 'default',
+        source: '1',
+        target: '2',
+        label: 'Edge Text'
+      }
+    ]);
+   
+  </script>
+   
+  <!--
+  👇 By default, the Svelte Flow container has a height of 100%.
+  This means that the parent container needs a height to render the flow.
+  -->
+  <div class="h-screen">
+    <SvelteFlow
+    nodeTypes={
+        {
+            custom: CustomNode
+        }
+    }
+      {nodes}
+      {edges}
+      fitView
+      panOnDrag={false}
+      on:nodeclick={(event) => console.log('on node click', event.detail.node)}
+    >
+      <Controls />
+      <Background variant={BackgroundVariant.Dots} />
+      <MiniMap />
+    </SvelteFlow>
+  </div>
